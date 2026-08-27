c87580529e76ae69c86234fde2d8f14115402a16
Carlos O'Ryan <coryan@google.com>
2026-08-27 17:34:23 +0000

    cleanup: rename `packages/*` directories  (#548)
    Make the directories match their future split repositories.

--- Package.swift
+++ Package.swift
@@ -24,10 +24,10 @@ let package = Package(
     .library(name: "GoogleCloudWorkloadManagerV1", targets: ["GoogleCloudWorkloadManagerV1"])
   ],
   dependencies: [
-    .package(path: "../../packages/auth"),
-    .package(path: "../../packages/gax"),
+    .package(path: "../../packages/swift-google-auth"),
+    .package(path: "../../packages/swift-google-gax"),
     .package(path: "../../generated/swift-google-cloud-location"),
-    .package(path: "../../packages/wkt"),
+    .package(path: "../../packages/swift-google-wkt"),
     .package(path: "../../generated/swift-google-longrunning"),
     .package(path: "../../generated/swift-google-rpc"),
     .package(url: "https://github.com/apple/swift-log", from: "1.12.0"),
@@ -36,10 +36,10 @@ let package = Package(
     .target(
       name: "GoogleCloudWorkloadManagerV1",
       dependencies: [
-        .product(name: "GoogleCloudAuth", package: "auth"),
-        .product(name: "GoogleCloudGax", package: "gax"),
+        .product(name: "GoogleCloudAuth", package: "swift-google-auth"),
+        .product(name: "GoogleCloudGax", package: "swift-google-gax"),
         .product(name: "GoogleCloudLocation", package: "swift-google-cloud-location"),
-        .product(name: "GoogleCloudWKT", package: "wkt"),
+        .product(name: "GoogleCloudWKT", package: "swift-google-wkt"),
         .product(name: "GoogleLongRunning", package: "swift-google-longrunning"),
         .product(name: "GoogleRpc", package: "swift-google-rpc"),
         .product(name: "Logging", package: "swift-log"),
